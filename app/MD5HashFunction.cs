013ed390588ef4177fb572dfbd458408563a43ae
waterb <143908720+belplaton@users.noreply.github.com>
2025-05-12 14:46:59 +0300

    Create MD5HashFunction.cs

---
+++ app/MD5HashFunction.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace waterb.app;
+
+public class MD5HashFunction<TProcessableType> : IHashFunction<TProcessableType>
+{
+    public string HashName => "md5";
+    public uint Process(TProcessableType value)
+    {
+        var md5 = MD5.Create();
+        var hash = md5.ComputeHash(Encoding.ASCII.GetBytes(value?.ToString() ?? string.Empty));
+        return BitConverter.ToUInt32(hash, 0);
+    }
+}
